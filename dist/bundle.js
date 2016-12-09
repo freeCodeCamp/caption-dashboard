096450b4128ea80750b7c1c1e431965f204992f4
theflametrooper <theflametrooper@gmail.com>
2016-12-09 18:08:58 +0000

    Allowed for pulling caption IDs for a specific video

--- dist/bundle.js
+++ dist/bundle.js
@@ -56,6 +56,36 @@
 
 	console.log('YouTube module successfully started');
 
+	var config = {
+	    apiKey: 'AIzaSyChV1Rsp9PgfPltS4DAu2GBLsUsgAY3cHc',
+	    clientId: '101706023134-e3890ujf30bjqqr7vjatmulpq6cr58cn.apps.googleusercontent.com.apps.googleusercontent.com'
+	};
+
+	var listCaptionsFromVideo = function listCaptionsFromVideo(videoId) {
+	    gapi.client.init({
+	        'apiKey': config.apiKey,
+	        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+	        'clientId': config.clientId,
+	        'scope': 'profile'
+	    }).then(function () {
+	        return gapi.client.youtube.captions.list({
+	            videoId: videoId,
+	            part: 'id'
+	        });
+	    }).then(function (response) {
+	        console.log(response.result);
+	    }, function (reason) {
+	        console.log('Error: ' + reason.result.error.message);
+	    });
+	};
+
+	gapi.load('client');
+
+	setTimeout(function () {
+	    listCaptionsFromVideo('v8kFT4I31es');
+	    downloadCaptionFromCaptions();
+	}, 500);
+
 /***/ },
 /* 2 */
 /***/ function(module, exports) {
